84466e80e048dcb123a171a60162783e0f0bdb18
jpacsai <judit.pacsai@gmail.com>
2018-10-18 16:25:36 +0200

    feat: click on list item makes it gray

--- src/ListItem.js
+++ src/ListItem.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class ListItem extends Component {
 
     constructor(props) {
-	super(props);
-	this.state = { color: 'black' };
+      super(props);
+      this.handleClick = this.handleClick.bind(this)
+      this.state = { color: 'black' };
     }
 
     handleClick() {
-	// Implement this function!
+      const newColor = this.state.color === 'black' ? 'gray' : 'black';
+
+      this.setState({
+        color: newColor
+      })
     }
 
   render() {
@@ -16,8 +21,8 @@ class ListItem extends Component {
     var name = item.name;
 
     return (
-	    <span onClick={this.handleClick.bind(this)} style={{color: this.state.color}}>
-        <strong>{name}</strong>
+      <span onClick={ () => this.handleClick() } style={{color: this.state.color}}>
+        <strong>{item}</strong>
       </span>
     );
 
