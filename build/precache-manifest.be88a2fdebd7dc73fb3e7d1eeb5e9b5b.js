8ffcb96f20a663e7d8b5c63ff2eba9c29b152dc6
jpacsai <judit.pacsai@gmail.com>
2018-10-20 23:46:31 +0200

    chore: build

---
+++ build/precache-manifest.be88a2fdebd7dc73fb3e7d1eeb5e9b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52e464fc43844ba8b4cb",
+    "url": "/List_tracker/static/js/runtime~main.52e464fc.js"
+  },
+  {
+    "revision": "4a86caf7a7c2013af7f9",
+    "url": "/List_tracker/static/js/main.4a86caf7.chunk.js"
+  },
+  {
+    "revision": "f06fdf6f22bca66abc30",
+    "url": "/List_tracker/static/js/1.f06fdf6f.chunk.js"
+  },
+  {
+    "revision": "4a86caf7a7c2013af7f9",
+    "url": "/List_tracker/static/css/main.2210edd4.chunk.css"
+  },
+  {
+    "revision": "3a9451c653a42a5314835e091d7ba8b8",
+    "url": "/List_tracker/index.html"
+  }
+];
